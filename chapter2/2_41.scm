d7244c73b868a62f18b6997150e8baa919f18b90
YuLiu <yuliu314@outlook.com>
2013-05-26 23:07:40 +0800

    2-40-41

--- chapter2/2_41.scm
+++ chapter2/2_41.scm
@@ -57,14 +57,3 @@
 
 (define (flatmap proc seq)
   (accumulate append '() (map proc seq)))
-
-(define (prime-sum? pair)
-  (prime? (+ (car pair) (cadr pair))))
-
-(define (make-pair-sum pair)
-  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
-
-(define (enumerate-interval low high)
-  (if (> low high)
-	'()
-	(cons low (enumerate-interval (+ low 1) high))))
